bf4a1166d14838191b370c06ed26d8c6b75832ec
krivtsun <krivtsuna@gmail.com>
2021-06-11 16:54:01 +0300

    Styl fixed

--- src/index.js
+++ src/index.js
@@ -395,4 +395,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
     const cursor = new Cursor(element, target);
 
+
+    // Проверяем, можно ли использовать Webp формат
+    function canUseWebp() {
+        let elem = document.createElement('canvas');
+        if (!!(elem.getContext && elem.getContext('2d'))) {
+            return elem.toDataURL('img').indexOf('data:img') == 0;
+        }
+        return false;
+    }
+    if (canUseWebp()) {
+        let images = document.querySelectorAll('[data-bg]');
+        for (let i = 0; i < images.length; i++) {
+            let image = images[i].getAttribute('data-bg');
+            images[i].style.backgroundImage = 'url(' + image + ')';
+        }
+        let isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+        let firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
+        if (canUseWebp() || firefoxVer >= 65) {
+            let imagesWebp = document.querySelectorAll('[data-bg-webp]');
+            for (let i = 0; i < imagesWebp.length; i++) {
+                let imageWebp = imagesWebp[i].getAttribute('data-bg-webp');
+                imagesWebp[i].style.backgroundImage = 'url(' + imageWebp + ')';
+            }
+        }
+    }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -15,18 +15,17 @@ module.exports = {
     },
 
     plugins: [new MiniCssExtractPlugin(),
-        // new PurgeCSSPlugin({
-        //     paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-        // }),
+
     ],
     module: {
         rules: [
+         
             {
                 test: /\.scss$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }, {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|webp)$/,
                 use: [{
                     loader: 'url-loader',
                     options: {
@@ -34,7 +33,7 @@ module.exports = {
                         name: '[path][name].[ext]?hash=[hash:20]',
                         limit: 8192
                     }
-                }]
+                },]
             },
             {
                 // Load all icons
@@ -51,20 +50,8 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [
-            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-            // `...`,
             new CssMinimizerPlugin(),
         ],
-        // splitChunks: {
-        //     cacheGroups: {
-        //         styles: {
-        //             name: 'styles',
-        //             test: /\.css$/,
-        //             chunks: 'all',
-        //             enforce: true
-        //         }
-        //     }
-        // }
     },
     stats: {
         children: true,
