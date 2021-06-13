e0e0026f36af2192d1965a7e5914634076e865b5
krivtsuna <krivtsuna@gmail.com>
2021-06-13 17:45:02 +0300

    Bugfix more

--- src/index.js
+++ src/index.js
@@ -172,8 +172,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
         let arrayOfElement = centerCoordinatesOfItem.slice()
         onmousemove = function(e) {
             let x = window.innerWidth;
-            e.clientX - window.innerWidth / 2 > 0 ? x = e.clientX - 60 : x = e.clientX - 200
-
+            console.log(window.innerWidth)
+            if (window.innerWidth != 1562) {
+                e.clientX - window.innerWidth / 2 > 0 ? x = e.clientX - 60 : x = e.clientX - 200
+            } else {
+                x = e.clientX
+            }
 
             let closest = centerCoordinatesOfItem.sort((a, b) => Math.abs(x - a) - Math.abs(x - b))[0]
             let indexOfGotScale = arrayOfElement.indexOf(closest);
